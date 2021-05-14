ea9334171a8edb69131f6f451ad3e7110ecb4952
DPSystem <drop10@hotmail.es>
2021-05-14 09:39:27 -0300

    Agregar archivos de proyecto.

---
+++ SindicatoDB/dbo/Tables/Table.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Table] (
+    [Id]       INT            IDENTITY (1, 1) NOT NULL,
+    [Nombre]   VARCHAR (50)   NULL,
+    [Apellido] VARBINARY (50) NULL,
+    [UserId]   INT            NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
